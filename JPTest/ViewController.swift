f2a62f6fcdf9f44c73c7c71cc01db6b44aaefcc5
Insu Park <facessu@gmail.com>
2020-03-23 17:17:50 +0900

    Initial Commit

---
+++ JPTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JPTest
+//
+//  Created by Insu Park on 2020/03/23.
+//  Copyright © 2020 INSWAG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
